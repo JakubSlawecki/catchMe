3810a5b5bdb840a55b3663fb776176b2d2909cd5
Jakub Slawecki <jakubslawecki@gmail.com>
2018-09-05 19:50:13 +0200

    lego prisoner 3d model added

--- catchMe/ViewController.swift
+++ catchMe/ViewController.swift
@@ -20,16 +20,40 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
         self.sceneView.autoenablesDefaultLighting = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     @IBAction func play(_ sender: Any) {
-        
+        self.addNode()
     }
     
     @IBAction func reset(_ sender: Any) {
         
     }
     
+    func addNode() {
+        let legoPrisonerScene = SCNScene(named: "art.scnassets/LEGO Minifigure_Export for COLLADA 1.4.1 (LOW POLY).scn")
+        let legoPrisonerNode = legoPrisonerScene?.rootNode.childNode(withName: "LegoPrisoner", recursively: false)
+        legoPrisonerNode?.position = SCNVector3(0, 0, -0.5)
+        self.sceneView.scene.rootNode.addChildNode(legoPrisonerNode!)
+
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTapped = sender.view as! SCNView
+        let touchedCoordinates = sender.location(in: sceneViewTapped)
+        let hitTest = sceneViewTapped.hitTest(touchedCoordinates)
+        if hitTest.isEmpty {
+             print("sceneView tapped")
+        } else {
+            let result = hitTest.first!
+            let geometry = result.node.geometry
+            print(geometry)
+        }
+       
+        
+    }
 
 }
 
